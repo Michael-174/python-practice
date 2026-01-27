7a40f294df7086bbedebc916318a55b529915e34
Michael-174 <142183301+Michael-174@users.noreply.github.com>
2026-01-26 21:57:11 -0500

    generator

--- Learning/List/List Comprehensions.py
+++ Learning/List/List Comprehensions.py
@@ -1,19 +1,27 @@
-a = [a * a for a in range(1,40) if a % 2 == 0]
-print(a) #循环生成偶数幂
+# a = [a * a for a in range(1,40) if a % 2 == 0]
+# print(a) #循环生成偶数幂
 
-b = [m+n for m in "nigga" for n in "nigga"]
-print(b) #二层循环声称list
+# b = [m+n for m in "nigga" for n in "nigga"]
+# print(b) #二层循环声称list
 
-c = {"a":1, "b":2, "c":3, "d":4, "e":5}
-c = [f"{x} = {y}"for x,y in c.items()]
-print(c) #循环打印dict
+# c = {"a":1, "b":2, "c":3, "d":4, "e":5}
+# c = [f"{x} = {y}"for x,y in c.items()]
+# print(c) #循环打印dict
 
-a = "nigga"
+# import os
+# scan = [x for x in os.listdir('.')]
+# print(scan)
 
-if isinstance(a, str): #use isinstance to check type
-    print(f"{a} is a string")
+# Capital = ["A", "B", "C", "D", "E", "F"]
+# Not_Capital = [a.lower() for a in Capital]
+# print(Not_Capital)
 
+# a = "nigga"
+#
+# if isinstance(a, str): #use isinstance to check type
+#     print(f"{a} is a string")
+#
 # L2 期待输出: ['hello', 'world', 'apple']
 L1 = ['Hello', 'World', 18, 'Apple', None]
 L2 = [a for a in L1 if isinstance(a, str)]
-print(L2) 
+print(L2)
--- Learning/List/generater.py
+++ Learning/List/generater.py
@@ -8,15 +8,13 @@
 # for x in L2:
 #     print(x)
 
-def fib(max):
-    n, a, b = 0, 0, 1
-    while n < max:
-        print(b)
-        a, b = b, a + b
-        n = n + 1
-    return 'done'
-
-
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         print(b)
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done'
 
 # def fib(total):
 #     fibonacci = [0, 1]
@@ -38,6 +36,47 @@ def fib(max):
 #
 # print(fib(t))
 
+# def fib(max):
+#     n, a, b = 0, 0, 1
+#     while n < max:
+#         yield b
+#         a, b = b, a + b
+#         n = n + 1
+#     return 'done' #如果一个函数定义中包含yield关键字，那么这个函数就不再是一个普通函数，而是一个generator
+# g = fib(6)
+# while True:
+#     try:
+#        x = next(g)
+#        print('g:', x)
+#     except StopIteration as e:
+#         print('Generator return value:', e.value)
+#         break #试图显示return值
 
+# def odd():
+#     print("step 1")
+#     yield 1
+#     print("step 2")
+#     yield 2
+#     print("step 3")
+#     yield 3
+# o = odd()
+# print(next(o))
+# print(next(o))
+
+#杨辉三角
+def Ytrangle(max):
+    n, a, b = 0, 1, 1
+    level = [a]
+    while n < max:
+        yield level
+        a = level.copy()
+        a.append(b)
+        for i in range(1, len(level)):
+            a[i] = level[i-1] + level[i]
+        level = a.copy()
+        n = n + 1
+
+for i in Ytrangle(100):
+    print(i)
 
 
